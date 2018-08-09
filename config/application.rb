62fa0cdf8513ec164c566231bcaf10f62ad9116a
¨anniezh12¨ <aniqaelahi@yahoo.com>
2018-08-09 16:33:04 -0700

    change config.assets.initialize_on_precompile to true in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Speakerinfo
     config.load_defaults 5.1
     config.assets.enabled = true
     # config/application.rb
-config.assets.initialize_on_precompile = false
+config.assets.initialize_on_precompile = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
