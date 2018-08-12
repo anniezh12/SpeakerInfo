094ba93a23613d9edc40f02909549831b52ffc7d
¨anniezh12¨ <aniqaelahi@yahoo.com>
2018-08-11 18:36:37 -0700

    configuring environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,12 +15,12 @@ Rails.application.configure do
     { :host => 'localhost3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              'smtp.mail.com',
     port:                 587,
-    domain:               'gmail.com',
+    domain:               'example.com',
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
-    authentication:       'login',
+    authentication:       'plain',
     enable_starttls_auto: true
   }
 
